4248b89c8f8e6512055d7975fd795dc87bff93a7
antonioespinoza98 <espinozamarco70@gmail.com>
2022-10-08 20:36:04 -0600

    Final commit

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 ## Create a basic shiny app
 
-+ This app will consists in a input and ouput source using Snowflake as a powercenter. I might use a trial account such as S3 bucket or Azure to store my data.  
++ This basically just shows a basic web app
--- app.py
+++ app.py
@@ -1,12 +1,38 @@
 from venv import create
-from shiny import App, ui, render
+from shiny import App, ui,  render
 
 # UI
 
 app_ui = ui.page_fluid(
+    ui.navset_pill(
+        ui.nav("About me",
+        ui.page_fluid(
+            ui.panel_title(
+                "Who am I?"
+            ),
+            ui.layout_sidebar(
+                ui.panel_sidebar(
+                    ui.output_ui("name"),
+                    ui.output_text("me")
+                ),
+                ui.panel_main(
+                    ui.output_ui("photo")
+                )
+            )
+        )
+        ),
 
-    ui.input_slider("n", "N", 0,100,40),
-    ui.output_text_verbatim("txt"),
+        ui.nav_control(
+            ui.a("GitHub", href = "https://github.com/antonioespinoza98", target = "_blank")
+            ),
+
+        ui.nav_control(
+            ui.a("Instagram", href = "https://www.instagram.com/marco_espinoza/", target = "_blank")
+            ),
+        ui.nav_control(
+            ui.a("LinkedIn", href = "https://www.linkedin.com/in/marco-antonio-espinoza-marin-5365a1176/", target="_blank")
+        )
+    )
 
 )
 
@@ -15,10 +41,20 @@ app_ui = ui.page_fluid(
 
 def server(input, output, session):
     @output
+    @render.ui
+
+    def name():
+        return ui.HTML("<marquee>My name is Marco Espinoza!</marquee>")
+
+    @output
     @render.text
+    def me():
+        return "I am currently working as a Data Analyst/Engineer. I am also a student finishing my bachelor's degree in statistics at the University of Costa Rica. My professional interests are data engineering and data analysis using complex statistical models. In addition, I have a great passion for music, so I like to play the guitar and go to local music venues. I also like art and I am in the process of learning how to paint on canvas because in the future I want to develop a project called Timeline that consists of a series of paintings that relate important memories in my life. I also have a great passion for analog photography."
 
-    def txt():
-        return f"n*2 is {input.n() * 2}"
+    @output
+    @render.ui
 
+    def photo():
+        return ui.HTML("<a href='https://pbs.twimg.com/media/FSg-V9UUUAAU6MO?format=jpg&name=small'><img src='https://pbs.twimg.com/media/FSg-V9UUUAAU6MO?format=jpg&name=small' alt='MDN' width='500' height='600'></a>")
 app = App(app_ui, server)
 
